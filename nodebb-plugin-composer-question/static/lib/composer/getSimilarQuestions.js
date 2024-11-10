c244e02376fb0da43dbb6f0623da39ff00024dda
anna-ayn <19-10096@usb.ve>
2024-11-10 15:41:45 -0400

    Se agrega visualizacion de preguntas similares al escribir en el titulo de una pregunta

--- nodebb-plugin-composer-question/static/lib/composer/getSimilarQuestions.js
+++ nodebb-plugin-composer-question/static/lib/composer/getSimilarQuestions.js
@@ -3,43 +3,50 @@
 define('composer/getSimilarQuestions', [], function () {
 	const getSimilarQuestions = {};
 
-    getSimilarQuestions.fetchSimilarQuestions = function (inputTitleQuestion) {
+    getSimilarQuestions.fetchSimilarQuestions = async function (inputTitleQuestion) {
         // se obtiene la categoria con cid = 5 que es Questions & Answers
-        $.get('/api/category/5', function (data) {
-            // se obtiene los topics de esta categoria
-            const allQuestions = data.topics;
-            
-            // si no hay topics en la categoria, se retorna
-            if (!allQuestions.length) {
-                return;
-            }
+        let allQuestions;
+        try {
+            const response = await $.getJSON(`/api/category/5`);
+            allQuestions = response;
+        } catch (error) {
+            console.error("Error al obtener las preguntas:", error);
+        }
 
-            // aqui se comparan las palabras del titulo de la pregunta actual con las palabras de los titulos de 
-            // preguntas anteriores
+        // se guardan las preguntas o topicos en allQuestions
+        allQuestions = allQuestions.topics;
 
-            // funcion para eliminar los acentos, convertirlos en minusculas
-            // y eliminar los caracteres especiales
-            const NormalizeWords = (str) => {
-                return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^\w\s]/gi, '');
-            };
+        // si no hay topicos, se retorna un array vacio
+        if (allQuestions.length === 0) {
+            return [];
+        }
 
-            const inputTitleQuestionWords = NormalizeWords(inputTitleQuestion).split(' ').map(word => word.toLowerCase());
-            const similarResults = allQuestions.filter(topic => {
-                const topicWords = NormalizeWords(topic.title).split(' ').map(word => word.toLowerCase());
-                const commonWords = topicWords.filter(word => inputTitleQuestionWords.includes(word));
-                // si existe al menos una palabra en comun, se agrega a los resultados
-                // agregar tambien la longitud de las palabras en comun para poder ordenar los resultados
-                topic.commonWordsCount = commonWords.length;
-                return commonWords.length > 0;
-            });
+        // aqui se comparan las palabras del titulo de la pregunta actual con las palabras de los titulos de 
+        // preguntas anteriores
 
-            // ordenar los resultados por la cantidad de palabras en comun
-            similarResults.sort((a, b) => b.commonWordsCount - a.commonWordsCount);
+        // funcion para eliminar los acentos, convertirlos en minusculas
+        // y eliminar los caracteres especiales
+        const NormalizeWords = (str) => {
+            return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^\w\s]/gi, '');
+        };
 
-            // mostrar los resultados en la consola
-            console.log(similarResults);
-            
+        const inputTitleQuestionWords = NormalizeWords(inputTitleQuestion).split(' ').map(word => word.toLowerCase());
+        const similarResults = allQuestions.filter(topic => {
+            const topicWords = NormalizeWords(topic.title).split(' ').map(word => word.toLowerCase());
+            const commonWords = topicWords.filter(word => inputTitleQuestionWords.includes(word));
+            // si existe al menos una palabra en comun, se agrega a los resultados
+            // agregar tambien la longitud de las palabras en comun para poder ordenar los resultados
+            topic.commonWordsCount = commonWords.length;
+            return commonWords.length > 0;
         });
+
+        // ordenar los resultados por la cantidad de palabras en comun
+        similarResults.sort((a, b) => b.commonWordsCount - a.commonWordsCount); 
+
+        // solo filtrar los primeros 3 resultados
+        similarResults.splice(3);
+        return similarResults;
+        
     };
 
 	return getSimilarQuestions;

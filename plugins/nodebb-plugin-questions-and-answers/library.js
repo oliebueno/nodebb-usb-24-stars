3c641922791c20a9c09ee6a88624f44d57587a51
Jesús Bovea <57044155+ChuyB@users.noreply.github.com>
2024-11-11 13:35:02 -0400

    Añade el envío de notificaciones (#89)
    Envía la notificación de que un post ha sido marcado como oficial a
    todos los seguidores de un tópico

--- plugins/nodebb-plugin-questions-and-answers/library.js
+++ plugins/nodebb-plugin-questions-and-answers/library.js
@@ -1,14 +1,31 @@
-'use strict';
+"use strict";
 
-const Posts = require.main.require('./src/posts');
-const routeHelpers = require.main.require('./src/routes/helpers');
+const Posts = require.main.require("./src/posts");
+const Topics = require.main.require("./src/topics");
+const routeHelpers = require.main.require("./src/routes/helpers");
 
 const plugin = module.exports;
 
 // Set the official status of a post
 const setOfficial = async (data) => {
 	const { pid, official } = data;
-	await Posts.setPostField(pid, 'official', official);
+	await Posts.setPostField(pid, "official", official);
+
+	// Notify topic followers
+	if (official) {
+		const topic = await Topics.getTopicData(data.tid);
+		const notificationData = {
+			...data,
+			content: `Answer marked as official: ${data.content}`,
+			topic,
+		};
+		await Topics.notifyFollowers(notificationData, null, {
+			type: "new-reply",
+			bodyShort: "Answer marked as official",
+			nid: `official_answer:tid:${notificationData.topic.tid}:pid:${notificationData.pid}`,
+			mergeId: `notifications:user-posted-to|${notificationData.topic.tid}`,
+		});
+	}
 };
 
 plugin.addApiRoute = async ({ router, middleware, helpers }) => {
@@ -17,8 +34,8 @@ plugin.addApiRoute = async ({ router, middleware, helpers }) => {
 	// Adds API route to toggle official status of a post
 	routeHelpers.setupApiRoute(
 		router,
-		'post',
-		'/posts/official/:pid',
+		"post",
+		"/posts/official/:pid",
 		middlewares,
 		async (req, res) => {
 			const { body } = req;
